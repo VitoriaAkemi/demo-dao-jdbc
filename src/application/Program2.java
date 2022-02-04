d74f8c737dedb2b37177f9dbab3303d4074b0c89
Vitoria Akemi <rm88077@fiap.com.br>
2022-02-04 17:28:00 -0300

    findAll department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,13 @@
 package application;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program2 {
 
@@ -14,10 +17,36 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-	System.out.println("=== TEST 1: seller findById ===");
+		System.out.println("=== TEST 1: department findById ===");
 		
 		Department department = departmentDao.findById(3);
 		System.out.println(department);		
+		
+		
+		
+		System.out.println("\n=== TEST 2: department findAll ===");
+		
+		List<Department> list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		System.out.println("\n=== TEST 4: seller insert =====");
+		
+		Department newDepartment = new Department(null, "Paint");
+		DepartmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		*/
+		
 
 	}
 
